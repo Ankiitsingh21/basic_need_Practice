2491310bbb4f4e32a6a48acc958c2b9e78c2b951
Ankit Singh <ankiitsingh21@gmail.com>
2025-06-12 22:52:40 +0530

    adding cors

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require( "express");
 
 const app = express();
 
+const cors= require("cors");
+
+app.use(cors());
+
 app.get("/hello",(req,res) =>{
     const a = req.query.a;
     const b= req.query.b;
