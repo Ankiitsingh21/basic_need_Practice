4b02a12c75d711feddd8aaefa1339fd570ee1c28
Ankit Singh <ankiitsingh21@gmail.com>
2025-06-13 22:29:16 +0530

    adding logic for calculating interest

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const cors= require("cors");
 
 app.use(cors());
 
-app.get("/hello",(req,res) =>{
+app.get("/Add",(req,res) =>{
     const a = req.query.a;
     const b= req.query.b;
     const sum= parseInt(a)+parseInt(b);
     return res.json({
         sum,
+    })
+})
+
+app.get("/Interest",(req,res)=>{
+    const principal = req.query.principal;
+    const rate = req.query.rate;
+    const time = req.query.time;
+    const Interest=(parseInt(principal)*parseInt(rate)*parseInt(time))/100;
+    return res.json({
+        Interest,
     })
 })
 
